d286dc08f9353e8df99ddcc3bb8bf5daef9beff5
Eduardo Ureta <eduardo.uretaca@gmail.com>
2023-12-29 16:57:56 -0300

    Fin Proyecto Título

--- _config/settings.py
+++ _config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&rz^j-!sd$819@_(rdb+fh0$skos1r-(0x0p&kx$q7l#2%z*84
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://proyectotitulo.azurewebsites.net","http://127.0.0.1:8000/"]
+ALLOWED_HOSTS = ["proyectotitulo.azurewebsites.net","127.0.0.1"]
 
 
 # Application definition
