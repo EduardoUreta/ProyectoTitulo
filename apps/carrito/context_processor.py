454fea4b8823ba9b64063a7c686ea8517ba5a62c
Eduardo Ureta <eduardo.uretaca@gmail.com>
2023-12-31 16:26:01 -0300

    Fin

--- apps/carrito/context_processor.py
+++ apps/carrito/context_processor.py
@@ -1,8 +1,7 @@
 def total_carrito(request):
     total = 0
-    if request.user.is_authenticated:
-        if "carrito" in request.session:
-            for key, value in request.session["carrito"].items():
-                total += int(value["acumulado"])
-    
+    if "carrito" in request.session:
+        for key, value in request.session["carrito"].items():
+            total += int(value.get("acumulado", 0))  # Manejo de valores faltantes
+            
     return {"total_boleta": total}
